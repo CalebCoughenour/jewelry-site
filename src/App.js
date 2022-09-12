dd0b5aa49a187c827e08ada75bdf386db6f36f9b
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-12 10:51:01 -0700

    add routes for home, products and order

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/products' exact element={<Products />} />
+          <Route path='/order' exact element={<Order />} />
+        </Routes>
+      </Router>
     </>
   );
 }
