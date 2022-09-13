97896a732cb5cd6ae79d3a5ff58527790e665d2e
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-13 11:24:53 -0700

    add Header and Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Order from './pages/Order/Order';
 import Products from './pages/Products/Products';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
@@ -9,11 +11,13 @@ function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path='/' exact element={<Home />} />
           <Route path='/products' exact element={<Products />} />
           <Route path='/order' exact element={<Order />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
