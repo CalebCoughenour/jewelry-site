c11d4d5e7b046c02c84dcd372011a13efc2c7644
delcidj22 <jdelcid23@gmail.com>
2022-09-13 10:10:16 -0700

    Initiate order form page. Still working on adding and organizing

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,9 +1,27 @@
 import React from "react";
+import './Order.css';
+import photo1 from './orderForm.jpg';
 
 const Order = () => {
   return (
     <>
-    
+    <div className="col-sm-3">
+      <div className="HtoHeader">
+       <h1>Bhodi Jewlery </h1>
+        <h3>How to Order: </h3>
+      <div className="Instructions">
+        <ol>
+          <li>Download PDF Form below</li>
+          <li>Fill form out</li>
+          <li>Email form to mette@BohdiJewelry.net</li>
+        </ol>
+        </div>
+      </div>
+    </div>
+
+    <div className="col-sm-3">
+      <img src={photo1} alt="OrderForm" width="600" height="600" />
+      </div>
     </>
   )
 };
