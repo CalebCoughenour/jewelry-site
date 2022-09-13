740a84221a6c47161576fe317c08ddcb1d5acdd9
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-13 11:24:31 -0700

    add footer with logo and styling

---
+++ src/components/Footer/Footer.elements.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const FooterSec = styled.div`
+  padding-bottom: 15px;
+  background: gray;
+  border-top: 1px solid black;
+  width: 100%;
+`;
+
+export const FooterTitle = styled.a`
+  font-size: 25px;
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+
+  &:hover {
+    color: rgba(0, 0, 0, 0.6);
+    transition: 0.3s ease-out;
+  }
+`;
+
+export const FooterLink = styled.a`
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+  margin-left: .5rem;
+  margin-right: .5rem;
+
+  &:hover {
+    color: rgba(0, 0, 0, 0.6);
+    transition: 0.3s ease-out;
+  }
+`;
+
+export const FooterLogo = styled.img`
+  cursor: pointer;
+`;
+
+export const FooterEmail = styled.a`
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+  margin-left: .5rem;
+  margin-right: .5rem;
+  word-break: column;
+  
+
+  &:hover {
+    color: rgba(0, 0, 0, 0.6);
+    transition: 0.3s ease-out;
+  }
+`
+
+export const FooterPhoneNumber = styled.a`
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+  margin-left: .5rem;
+  margin-right: .5rem;
+  display: column;
+
+  &:hover {
+    color: rgba(0, 0, 0, 0.6);
+    transition: 0.3s ease-out;
+  }
+`;
