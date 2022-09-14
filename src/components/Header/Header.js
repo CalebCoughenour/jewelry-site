8089c8b102499f656373ca402ad5367298dfecd4
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-14 12:01:11 -0700

    add mail icon and update links for email

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { HeaderEmail, HeaderLogo, HeaderTitle } from './Header.elements';
+import { AiOutlineMail} from 'react-icons/ai';
 
 const Header = () => {
   return (
@@ -10,7 +11,7 @@ const Header = () => {
       <Navbar collapseOnSelect expand="md" style={{ background: '#7368A3', borderBottom: '1px solid black'}} >
         <Container>
           <HeaderLogo src='images/logo/mandala-icon-footer-header.webp' alt='A hand drawn mandala' />
-          <HeaderTitle>Bodhi Jewelry</HeaderTitle>
+          <HeaderTitle href='/'>Bodhi Jewelry</HeaderTitle>
           <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
@@ -18,7 +19,8 @@ const Header = () => {
               <Nav.Link href="/products">Products</Nav.Link>
               <Nav.Link href="/order">Order</Nav.Link>
             </Nav>
-            <HeaderEmail>mette@BodhiJewelry.net</HeaderEmail>
+            <AiOutlineMail />
+            <HeaderEmail href="mailto: mette@BodhiJewelry.net">mette@BodhiJewelry.net</HeaderEmail>
           </Navbar.Collapse>
         </Container>
       </Navbar>
