c60e483f61b90b517a8d03745294c59a5ff63281
delcidj22 <jdelcid23@gmail.com>
2022-09-16 19:14:54 -0700

    Change pdf route

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -5,22 +5,27 @@ import photo1 from './orderForm.jpg';
 const Order = () => {
   return (
     <>
-    <div className="col-sm-3">
-      <div className="HtoHeader">
-       <h1>Bhodi Jewlery </h1>
-        <h3>How to Order: </h3>
-      <div className="Instructions">
-        <ol>
-          <li>Download PDF Form Here: <a href="http://bodhijewelry.net/media/6523558b99acea0bffff81c7ffffe905.pdf" target ="_blank"><i class="fa-solid fa-download"></i></a></li>
-          <li>Fill form out</li>
-          <li>Email form to mette@BohdiJewelry.net</li>
-        </ol>
+      <div className="home-background">
+      <div className="card w-50 mb-3" style={{ background: '#897fb1f0', border: '1px solid black', borderRadius: '5px'}}>
+        <div className="col-sm-3">
+          <div className="HtoHeader">
+            <h1>Bhodi Jewlery </h1>
+            <h3>How to Order: </h3>
+            <div className="Instructions">
+              <ol>
+                <li>Download PDF Form Here: <a href={require("./orderform.pdf")} target="_blank" rel="noopener noreferrer"> <i class="fa-solid fa-download"></i></a></li>
+                <li>Fill form out</li>
+                <li>Email form to mette@BohdiJewelry.net</li>
+              </ol>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="col-sm-3">
-      <img src={photo1} alt="OrderForm" width="600" height="600" />
+
+        <div className="orderImage">
+          <img src={photo1} alt="OrderForm" width="600" height="600" />
+        </div>
       </div>
     </>
   )
